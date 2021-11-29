7ee07e7c562dd111a6642b25a72977213076e0db
Dina <dinek1983@mail.ru>
2021-11-29 19:05:54 +0300

    dz poem

---
+++ app/src/main/java/com/dina/myfirstapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dina.myfirstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "Наша мама\nПопросила\nТри минуты тишины!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Неужели\nНашей\nмаме")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Больше дети не нужны?!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Вот и всё -\nНикто не скачет")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Ни борьбы,\nНи кутерьмы")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Только кто-то\nГорько плачет!\nМама,слышишь?\nЭто мы!")
+    }
+}
